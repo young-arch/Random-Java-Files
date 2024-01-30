f6843252986df113453171325cfb066671528a51
young-arch <mawulegabriel@gmail.com>
2024-01-30 22:55:20 +0000

    Initial commit

---
+++ aaDemo.java
@@ -0,0 +1,9 @@
+public class aaDemo {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 6, 6, 6, 6, 7, 20 };
+        aa solution = new aa();
+        int result = solution.findSpecialInteger(arr);
+        System.out.println("Output: " + result);
+    }
+
+}
