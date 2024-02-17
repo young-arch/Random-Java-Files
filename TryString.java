6e0c2c3ac842bc2b64fd091b49055992c58a3169
young-arch <mawulegabriel@gmail.com>
2024-02-17 13:21:43 +0100

    Initial commit

---
+++ TryString.java
@@ -0,0 +1,13 @@
+public class TryString{
+    public static void main(String args[]){
+
+        String name = new String("navin");
+
+        System.out.println(name);
+        System.out.println(name.hashCode());
+        System.out.println(name.concat( " love"));
+        System.out.println(name.charAt(1));
+
+    }
+    
+}
