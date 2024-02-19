e989377b616270b66ae685d018d01c5d9dc360e4
young-arch <mawulegabriel@gmail.com>
2024-02-19 03:28:50 +0100

    Initial commit

---
+++ StockDemo1.java
@@ -0,0 +1,13 @@
+public class StockDemo1 {
+    public static void main(String[] args) {
+        // Create the stock object for the XYZ company.
+        // The trading symbol is XYZ and the crrent
+        // price per share is $9.62
+
+        Stock xyzCompany = new Stock("XYZ", 962);
+
+        System.out.println(xyzCompany);
+
+    }
+
+}
