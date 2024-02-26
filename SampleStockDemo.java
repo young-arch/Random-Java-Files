a857987ac9a9e0c5b83157cb29f734e92a07af0d
young-arch <mawulegabriel@gmail.com>
2024-02-26 16:50:48 +0100

    Initial commit

---
+++ SampleStockDemo.java
@@ -0,0 +1,9 @@
+public class SampleStockDemo {
+
+    public static void main(String[] args) {
+
+        Stock xyzCompany = new Stock("XYZ", 9.72);
+
+        System.out.println(xyzCompany);
+    }
+}
