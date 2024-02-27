3303c559591774f80753b3a7d28f49f4d2a0ef01
young-arch <mawulegabriel@gmail.com>
2024-02-27 16:49:18 +0100

    Initial commit

---
+++ SampleMet.java
@@ -0,0 +1,13 @@
+public class SampleMet {
+
+    public static double milesToKilometer(double m) {
+
+        return m * 1.079;
+    }
+
+    public static double kilometersTomile(double k) {
+
+        return k / 1.709;
+    }
+
+}
