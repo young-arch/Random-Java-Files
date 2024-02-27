0af2a590fc84135d89410b7905b181df32f02cfc
young-arch <mawulegabriel@gmail.com>
2024-02-28 00:35:23 +0100

    Initial commit

---
+++ ReplaceMethod.java
@@ -0,0 +1,14 @@
+public class ReplaceMethod {
+    public static void main(String[] args) {
+
+        String str = "  Tom Talbert Tried Trains  ";
+        String str2 = str.replace('T', 'D');
+        String str3 = str.trim();
+
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str3);
+
+    }
+
+}
