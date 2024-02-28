93e147d9704e0936177ccd801b17536db8815aeb
young-arch <mawulegabriel@gmail.com>
2024-02-28 01:36:51 +0100

    Initial commit

---
+++ QuaterlySalesStat.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class QuaterlySalesStat {
+    public static void main(String[] args){
+
+        int[][] sales = new int[6][6];
+        Scanner input = new Scanner(System.in); 
+        
+        System.out.println("Enter four quaterly sales figures for six divisons of the company ");
+
+        for(int index=0; index<sales.length-1; index++){
+            for(int i = 0; i<sales.length; i++){ 
+                
+                System.out.println("Division " + (index+1) )
+
+                
+            } 
+        }
+
+        
+
+
+
+    }
+
+}
