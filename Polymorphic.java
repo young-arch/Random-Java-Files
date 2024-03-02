6acc0a78dec438c78faa39ce363e4554b033fb52
young-arch <mawulegabriel@gmail.com>
2024-03-02 01:28:28 +0100

    Initial commit

---
+++ Polymorphic.java
@@ -0,0 +1,14 @@
+public class Polymorphic {
+    public static void main(String[] args){
+
+        GradeActivity[] tests = new GradeActivity[3];
+
+        tests[0] = new GradeActivity();
+
+        tests[0].setScore(95);
+
+        tests[1] = new PassFailActivity(0)
+
+    }
+
+}
