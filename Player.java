339c96216f6d3f43d198fd152712ea042d817d2f
young-arch <mawulegabriel@gmail.com>
2024-12-23 03:49:14 +0100

    Initial commit

---
+++ Player.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Player {
+    private String name;
+    private String guess;
+    private int points;
+
+    public Player(String playerName) {
+        name = playerName;
+        guess = " ";
+        points = 0;
+
+    }
+
+    public void makeGuess() {
+        Random rand = new Random();
+        int guessNumber = rand.nextInt(2);
+
+        if (guessNumber == 0) {
+            guess = "Cho (even)";
+        } else {
+            guess = "Han (odd)";
+        }
+
+    }
+
+    public void addPoints(int newPoints) {
+        points += newPoints;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+}
